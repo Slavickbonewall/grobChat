12dc9aabd380f59c43ba522c3789af334c7a890e
vladislavByte <132852713+Slavickbonewall@users.noreply.github.com>
2024-08-16 00:46:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
-
+const xss = require('xss');
+const PORT = process.env.PORT || 8080;
+const server = new WebSocket.Server({ port: PORT });
 server.on('connection', ws => {
     console.log('Nuevo cliente conectado');
-
     ws.on('message', message => {
         console.log('Recibido:', message);
-        
+        const sanitizedMessage = xss(message);
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                client.send(sanitizedMessage);
             }
         });
     });
-
-    ws.send('Bienvenido al chat!');
+    ws.send('Bienvenido a Un lugar sin censura');
 });
+console.log(`Servidor WebSocket escuchando en el puerto ${PORT}`);
