c6987079bd2b30c8404167124d1feb76262453b3
vladislavByte <132852713+Slavickbonewall@users.noreply.github.com>
2024-08-16 03:51:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const ws = new WebSocket(`https://grobchat.onrender.com`);
-    ws.onopen = () => {
-        console.log('Conectado al servidor WebSocket');
-    };
-    ws.onmessage = (event) => {
-        const messagesDiv = document.getElementById('messages');
-        const newMessage = document.createElement('div');
-        newMessage.classList.add('message');
+ let username = prompt('Por favor, ingrese su nombre de usuario:'); 
 
-        if (event.data instanceof Blob) {
-            const reader = new FileReader();
-            reader.onload = () => {
-                newMessage.textContent = reader.result; 
-                messagesDiv.appendChild(newMessage);
+        document.addEventListener('DOMContentLoaded', () => {
+            const ws = new WebSocket('https://grobchat.onrender.com');
+
+            ws.onopen = () => {
+                console.log('Conectado al servidor WebSocket');
             };
-            reader.readAsText(event.data);
-        } else {
-            newMessage.textContent = event.data; 
-            messagesDiv.appendChild(newMessage);
-        }
-    };
 
-    document.getElementById('sendButton').onclick = () => {
-        const messageInput = document.getElementById('messageInput');
-        const message = messageInput.value;
-        if (message) {
-            const formattedMessage = `Tú: ${message}`;
-            ws.send(formattedMessage);
-            messageInput.value = '';
-        }
-    };
-});
+            ws.onmessage = (event) => {
+                const messagesDiv = document.getElementById('messages');
+                const newMessage = document.createElement('div');
+                newMessage.classList.add('message');
+
+                if (event.data instanceof Blob) {
+                    const reader = new FileReader();
+                    reader.onload = () => {
+                        newMessage.textContent = reader.result; 
+                        messagesDiv.appendChild(newMessage);
+                    };
+                    reader.readAsText(event.data);
+                } else {
+                    newMessage.textContent = event.data; 
+                    messagesDiv.appendChild(newMessage);
+                }
+            };
+
+            document.getElementById('sendButton').onclick = () => {
+                const messageInput = document.getElementById('messageInput');
+                const message = messageInput.value;
+                if (message) {
+                    const formattedMessage = `${username}: ${message}`; 
+                    ws.send(formattedMessage);
+                    messageInput.value = '';
+                }
+            };
+        });
